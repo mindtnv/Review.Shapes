0bb64399e0416d64f12d9558e324a128c58ac5bf
mindtnv <47498401+mindtnv@users.noreply.github.com>
2023-01-30 16:23:35 +0300

    Add Circle

---
+++ src/Review.Shapes.Tests/CircleTests.cs
@@ -0,0 +1,22 @@
+﻿using FluentAssertions;
+
+namespace Review.Shapes.Tests;
+
+[TestFixture]
+public class CircleTests
+{
+    public static object[] Circles =
+    {
+        new double[] {5},
+        new double[] {1},
+        new double[] {10},
+    };
+
+    [Test]
+    [TestCaseSource(nameof(Circles))]
+    public void AreaTest(double radius)
+    {
+        var circle = new Circle(radius);
+        circle.Area.Should().Be(Math.PI * radius * radius);
+    }
+}
---
+++ src/Review.Shapes/Circle.cs
@@ -0,0 +1,7 @@
+﻿namespace Review.Shapes;
+
+public record struct Circle(double Radius) : IShape
+{
+    public double Radius { get; } = Radius;
+    public double Area { get; } = Math.PI * Radius * Radius;
+}

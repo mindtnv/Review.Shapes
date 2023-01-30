ec708d7ff2182cb2e492eb41ed7d2423983d0a17
mindtnv <47498401+mindtnv@users.noreply.github.com>
2023-01-30 16:27:20 +0300

    Add Rectangle

---
+++ src/Review.Shapes.Tests/RectangleTests.cs
@@ -0,0 +1,22 @@
+﻿using FluentAssertions;
+
+namespace Review.Shapes.Tests;
+
+[TestFixture]
+public class RectangleTests
+{
+    public static object[] Rectangles =
+    {
+        new double[] {5, 5},
+        new double[] {1, 9},
+        new double[] {10, 3},
+    };
+
+    [Test]
+    [TestCaseSource(nameof(Rectangles))]
+    public void AreaTest(double a, double b)
+    {
+        var rectangle = new Rectangle(a, b);
+        rectangle.Area.Should().Be(a * b);
+    }
+}
---
+++ src/Review.Shapes/Rectangle.cs
@@ -0,0 +1,6 @@
+﻿namespace Review.Shapes;
+
+public record struct Rectangle(double A, double B) : IShape
+{
+    public double Area { get; } = A * B;
+}
